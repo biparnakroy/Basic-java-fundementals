169336c7a16dee587577b53b64ca676b6bab296d
biparnakroy <54219771+biparnakroy@users.noreply.github.com>
2019-08-20 03:59:13 +0530

    Create java_patterns_3.java

---
+++ patterns/java_patterns_3.java
@@ -0,0 +1,32 @@
+/*
+pattern:
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+*/
+//code:
+import java.io.*;
+class java_patt_1
+{
+  public static void main(String args[])throws IOException
+    {
+      InputStreamReader read =new InputStreamReader( new BufferedReader(System.in));
+      System.out.println("ENTER THE SIZE OF PATTERN");
+      int n = Integer.parseInt(in.readLine());
+      int i=0;j=0,k=0;
+      for(i=1;i<=n;i++)
+      {
+        for(j=1;j<=i;j++)
+        {
+          System.out.print(" ");
+          }
+         
+        for(k=n;k>=i;k--)
+        {
+          System.out.print("*");
+          }
+          System.out.println();
+       }
+    }
